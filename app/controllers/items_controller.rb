314ab0bdcd22db47e704d52a930cca17715b18c5
Jason Botesazan <jbotesazan@gmail.com>
2022-02-02 18:58:28 -0500

    commit tree be8e75d804b8f451b09078ad6ff9bf514fe1d84f

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,33 @@
 class ItemsController < ApplicationController
-
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
   def index
-    items = Item.all
-    render json: items, include: :user
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end
+    render json: items, include: :user, status: :ok
+  end
+
+  def show
+    item = Item.find(params[:id])
+    render json: item, status: :ok
+  end
+
+  def create
+      user = User.find(params[:user_id])
+      render json: user.items.create(item_params), status: :created
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :price)
+  end
+
+  def render_not_found_response
+    render json: { error: "Not found" }, status: :not_found
   end
 
 end
